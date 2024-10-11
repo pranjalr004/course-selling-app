e814537eae8e4484c825b91585b3bfd0f1d84b23
pranjalr004 <pranjalr376@gmail.com>
2024-10-11 19:17:58 +0530

    Step 5, Added the Schema

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const {adminRouter}=require("./routes/admin")
 const app=express()
 
 
-
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/admin",adminRouter)
 app.use("/api/v1/course",courseRouter)
